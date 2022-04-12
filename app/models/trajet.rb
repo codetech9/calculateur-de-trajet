26e4aa19571f1d8de1935679ff82283947519b61
Moïse GUANNEL <mguannel97@gmail.com>
2022-04-12 18:04:07 -0400

    upd courant

--- app/models/trajet.rb
+++ app/models/trajet.rb
@@ -53,21 +53,4 @@ class Trajet < ApplicationRecord
     response = https.request(request)
     JSON.parse(response.read_body, object_class: Hash)
   end
-
-
-  def emplacement
-    require "uri"
-    require "net/http"
-    ascii_origin = ActiveSupport::Inflector.transliterate(origin_addresse)
-    ascii_destination = ActiveSupport::Inflector.transliterate(destination_addresse)
-    url = URI("https://maps.googleapis.com/maps/api/place/queryautocomplete/json?input=#{ascii_origin}&input=#{ascii_destination}&country=mq&location=14.641528%2C-61.024174&radius=65000&language=fr&key=#{ENV['GOOGLE']}")
-
-    https = Net::HTTP.new(url.host, url.port)
-    https.use_ssl = true
-
-    request = Net::HTTP::Get.new(url)
-
-    response = https.request(request)
-    puts response.read_body
-  end
 end
